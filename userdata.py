0d9b2c9892f1c5cd2915c95c63012310c81b6d2a
afarrukh <afarrukh@ithaca.edu>
2024-05-28 14:54:51 -0400

    Commit

--- userdata.py
+++ userdata.py
@@ -1,5 +1,8 @@
 user_data = {
     "705131444": "Aanish Farrukh",
     "900103698": "Wyatt 'Danger' Maszczak",
-    "705162198": "Andrew Kaiser"
+    "705162198": "Andrew Kaiser",
+    "705140818": "Aslan Shapirov",
+    "":"",
+    "":""
 }
